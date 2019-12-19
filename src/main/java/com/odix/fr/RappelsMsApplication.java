821c39746005e6360b2957328e7cba2fb57f71ed
GitLabOdix <azaheni@smartgraphe.com>
2019-12-19 13:25:58 +0100

    first commit

---
+++ src/main/java/com/odix/fr/RappelsMsApplication.java
@@ -0,0 +1,13 @@
+package com.odix.fr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RappelsMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RappelsMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/odix/fr/RappelsMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odix.fr;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RappelsMsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
